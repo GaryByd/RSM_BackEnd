6aabe7ac7a5d6216edf8c5d43b028db7c2467809
luojiawei <2240774934@qq.com>
2024-09-03 16:21:18 +0800

    start task

---
+++ rsm/src/main/java/com/rc/controller/RsmPatrolItemController.java
@@ -0,0 +1,20 @@
+package com.rc.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 巡检项表 前端控制器
+ * </p>
+ *
+ * @author 罗佳炜
+ * @since 2024-09-03
+ */
+@RestController
+@RequestMapping("/rsm-patrol-item")
+public class RsmPatrolItemController {
+
+}
---
+++ rsm/src/main/java/com/rc/controller/RsmSnapshotController.java
@@ -0,0 +1,20 @@
+package com.rc.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 随手拍问题表 前端控制器
+ * </p>
+ *
+ * @author 罗佳炜
+ * @since 2024-09-03
+ */
+@RestController
+@RequestMapping("/rsm-snapshot")
+public class RsmSnapshotController {
+
+}
